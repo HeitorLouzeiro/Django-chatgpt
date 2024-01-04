3c59bc13959819f2a2f9ab343171d8b915df9915
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2024-01-04 15:13:25 -0300

    Update login page and site views.

--- chatbot/views/site.py
+++ chatbot/views/site.py
@@ -4,7 +4,7 @@ import os
 
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.decorators.csrf import csrf_exempt
 
 from .chatbot import carrega, trata_resposta
@@ -16,32 +16,28 @@ def index(request):
     return render(request, template_name)
 
 
+@login_required(login_url='accounts:login', redirect_field_name='next')
 @csrf_exempt
 def chat(request):
     if request.method == 'POST':
-        try:
-            data = json.loads(request.body.decode('utf-8'))
-            prompt = data['msg']
+        data = json.loads(request.body.decode('utf-8'))
+        prompt = data['msg']
 
-            nome_do_arquivo = request.user.username
+        nome_do_arquivo = request.user.username
 
-            historico = ''
-            if os.path.exists(nome_do_arquivo):
-                historico = carrega(nome_do_arquivo)
+        historico = ''
+        if os.path.exists(nome_do_arquivo):
+            historico = carrega(nome_do_arquivo)
 
-            response = list(trata_resposta(prompt, historico, nome_do_arquivo))
+        response = list(trata_resposta(prompt, historico, nome_do_arquivo))
 
-            # Return the result as a JsonResponse
-            return JsonResponse({'response': response})
-
-        except json.JSONDecodeError:  # noqua
-            return JsonResponse({'response': 'Invalid JSON format'}, status=400)
-
-    # If the request is not a POST request, render a template
-    template_name = 'pages/index.html'
-    return render(request, template_name)
+        # Return the result as a JsonResponse
+        return JsonResponse({'response': response})
+    else:
+        return redirect('chatbot:index')
 
 
+@login_required(login_url='accounts:login', redirect_field_name='next')
 @csrf_exempt
 def limpar_historico(request):
     nome_do_arquivo = request.user.username
